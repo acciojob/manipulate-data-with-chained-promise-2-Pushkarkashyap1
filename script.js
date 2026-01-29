4c64cd0605af8e8eb045398a3cde545dd57f123c
Pushkarkashyap1 <163689750+Pushkarkashyap1@users.noreply.github.com>
2026-01-29 11:50:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+const numbers = [1, 2, 3, 4];
+
+function filterEvenNumbers(arr) {
+    return new Promise((resolve) => {
+        const evens = arr.filter(num => num % 2 === 0);
+        document.getElementById("filterResult").textContent = evens;
+        resolve(evens);
+    });
+}
+
+function multiplyByTwo(arr) {
+    return new Promise((resolve) => {
+        const multiplied = arr.map(num => num * 2);
+        document.getElementById("multiplyResult").textContent = multiplied;
+        resolve(multiplied);
+    });
+}
+
+
+filterEvenNumbers(numbers)
+    .then(result => multiplyByTwo(result))
+    .catch(error => console.error(error));
+
